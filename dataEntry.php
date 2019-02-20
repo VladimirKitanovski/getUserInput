9922e1e0a707b8b87f9023e394b28fb8a649153d
Vladimir <vladimirkitanovski22@gmail.com>
2019-02-20 13:32:29 +0100

    Using the $_GET function

---
+++ dataEntry.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>PHP Programming Examples</title>
+  
+</head>
+<body>
+
+    <form action="dataEntry.php" method="get">
+    Name: <input type="text" name="username">
+    <br>
+    Age: <input type="number" name="age">
+    <br>
+            <input type="submit">
+            </form>
+            <br>
+            Your name is <?php echo $_GET["username"]; ?>
+            <br>
+            Your age is <?php echo $_GET["age"]; ?>
+
+</body>
+</html>
